ce7334baa9d57c5510cbc174bf3885395bf1582a
david <dborgat@gmail.com>
2024-04-14 17:54:01 -0300

    Refactor styling and optimize image loading in AsesoryPage and PlansPage components

--- src/app/contacto/page.tsx
+++ src/app/contacto/page.tsx
@@ -5,52 +5,50 @@ import Image from 'next/image';
 
 export default function Contact() {
   return (
-    <>
-      <div className="min-h-2 pb-20 lg:pb-12 px-6 lg:px-16 pt-24 max-w-full sky-gradient">
-        <div className="mt-3 md:mt-6 flex flex-col lg:gap-10 items-center">
-          <h1 className={`${poppins.className} text-4xl lg:text-6xl line-height-lg text-center text-primary font-black lg:pb-8 uppercase`}>
-            Contacto
-          </h1>
-          <div className="flex flex-col gap-8 max-w-7xl mb-4 lg:mb-12">
-            <h4 className={`${dmSans.className} text-center font-medium text-2xl md:text-4xl leading-10 text-accent`}>Consultorio presencial</h4>
-            <div className="flex flex-col bg-lilac card-blured p-8 shadow-lg rounded-xl">
-              <p className={`${poppins.className} text-lg`}>El consultorio está ubicado en Comodoro Rivadavia.</p>
-              <p className={`${poppins.className} text-lg`}>Avenida Rivadavia 845 - planta alta.</p>
-              <p className={`${poppins.className} text-lg`}>Consultorio 5 - planta baja y consultorio 3.</p>
-              <p className={`${poppins.className} text-lg`}>Zona centro</p>
-            </div>
-            <div className="my-8">
-              <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2778.4637348915403!2d-67.48935972380822!3d-45.862030971083286!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xbde45456ee6e20eb%3A0xe4bdf509b511471a!2sAv.%20Rivadavia%20845%2C%20U9000%20Comodoro%20Rivadavia%2C%20Chubut!5e0!3m2!1ses!2sar!4v1710984818407!5m2!1ses!2sar"
-                width="600"
-                height="450"
-                style={{ border: 0 }}
-                /*      allowfullscreen={true} */
-                loading="lazy"
-                referrerPolicy="no-referrer-when-downgrade"></iframe>
-            </div>
-            <div>
-              <h4 className={`${dmSans.className} text-center font-medium text-2xl md:text-4xl leading-10 text-accent mb-8`}>
-                Redes sociales y medios de contacto
-              </h4>
-              <ul className="flex items-center text-neutral sm:mt-0 justify-center space-x-6 card-blured p-4 rounded-xl gap-4 shadow-xl">
-                {socialMediaItems.map(({ id, href, icon, alt }) => (
-                  <div className="flex flex-col items-center" key={id}>
-                    <Link
-                      href={href}
-                      key={id}
-                      className="mb-2 inline-block rounded-full p-3 text-xs font-medium uppercase leading-normal text-white transition duration-150 ease-in-out hover:shadow-md cursor-pointer focus:ring-2 focus:ring-accent hover:ring-2 hover:ring-accent shadow-lg">
-                      <Image alt={alt} src={icon} />
-                    </Link>
-                    <p className="text-accent">{id}</p>
-                  </div>
-                ))}
-              </ul>
-            </div>
+    <div className="min-h-2 pb-20 lg:pb-12 px-6 lg:px-16 pt-24 max-w-full h-screen flex items-center md:justify-center sky-gradient">
+      <div className="mt-3 md:mt-6 flex flex-col lg:gap-10 items-center">
+        <h1 className={`${poppins.className} text-2xl md:text-4xl lg:text-6xl line-height-lg text-center text-primary font-black lg:pb-8 uppercase`}>
+          Contacto
+        </h1>
+        <div className="flex flex-col gap-8 max-w-7xl mb-4 lg:mb-12">
+          <h4 className={`${dmSans.className} text-center font-semibold md:font-medium text-xl md:text-4xl leading-10 text-accent`}>
+            Consultorio presencial
+          </h4>
+          <div className="flex flex-col bg-lilac card-blured p-4 md:p-8 shadow-lg rounded-xl self-center w-11/12 md:w-full">
+            <p className={`${poppins.className} md:text-lg`}>El consultorio está ubicado en Comodoro Rivadavia.</p>
+            <p className={`${poppins.className} md:text-lg`}>Avenida Rivadavia 845 - planta alta.</p>
+            <p className={`${poppins.className} md:text-lg`}>Consultorio 5 - planta baja y consultorio 3.</p>
+            <p className={`${poppins.className} md:text-lg`}>Zona centro</p>
+          </div>
+          <div className="md:my-8 md:w-full self-center">
+            <iframe
+              className="w-full md:h-96  rounded-xl shadow-xl"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2778.4637348915403!2d-67.48935972380822!3d-45.862030971083286!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xbde45456ee6e20eb%3A0xe4bdf509b511471a!2sAv.%20Rivadavia%20845%2C%20U9000%20Comodoro%20Rivadavia%2C%20Chubut!5e0!3m2!1ses!2sar!4v1710984818407!5m2!1ses!2sar"
+              /*      allowfullscreen={true} */
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"></iframe>
+          </div>
+          <div>
+            <h4 className={`${dmSans.className} hidden md:flex text-center font-medium text-2xl md:text-4xl leading-10 text-accent mb-8`}>
+              Redes sociales y medios de contacto
+            </h4>
+            <ul className="hidden md:flex items-center text-neutral sm:mt-0 justify-center space-x-6 card-blured p-4 rounded-xl gap-4 shadow-xl">
+              {socialMediaItems.map(({ id, href, icon, alt }) => (
+                <div className="flex flex-col items-center" key={id}>
+                  <Link
+                    href={href}
+                    key={id}
+                    className="mb-2 inline-block rounded-full p-3 text-xs font-medium uppercase leading-normal text-white transition duration-150 ease-in-out hover:shadow-md cursor-pointer focus:ring-2 focus:ring-accent hover:ring-2 hover:ring-accent shadow-lg">
+                    <Image alt={alt} src={icon} />
+                  </Link>
+                  <p className="text-accent">{id}</p>
+                </div>
+              ))}
+            </ul>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
--- src/app/sobre-mi/page.tsx
+++ src/app/sobre-mi/page.tsx
@@ -1,58 +1,72 @@
+'use client'; // Error components must be Client Components
+
 import { poppins, dmSans } from '../fonts';
 import Image from 'next/image';
 import profilePhoto from '../../../public/assets/consul02.jpeg';
-import avocadoToast from '../../../public/assets/avocadoToast.jpg';
+import consul from '../../../public/assets/consul.jpeg';
 import { especialidades } from '@/constants';
 
 export default function About() {
   return (
-    <>
-      <div className="min-h-2 pb-20 lg:pb-12 px-6 lg:px-16 pt-24 max-w-full">
-        <div className="mt-3 md:mt-6 flex flex-col lg:gap-10 items-center">
-          <h1 className={`${poppins.className} text-4xl lg:text-6xl line-height-lg text-center text-primary font-black  lg:pb-8 uppercase`}>
-            Sobre mí
-          </h1>
-          <div className="flex flex-col gap-8 max-w-6xl mb-4 lg:mb-12">
-            <div className="columns-1 lg:columns-2 gap-x-0">
-              <div className="flex-1 lg:flex justify-start  hidden">
-                <Image src={profilePhoto} alt="profile-photo-consultory" className="rounded-2xl" width={420} />
-              </div>
-              <div className="flex flex-col gap-7">
-                <h4 className={`${dmSans.className} text-center font-medium text-2xl md:text-4xl lg:text-left leading-10`}>
-                  ¡Hola! Soy Daiana Maldonado
-                </h4>
-                <h4 className={`${dmSans.className} text-center font-medium text-lg md:text-xl lg:text-left leading-10`}>
+    <div className="min-h-2 pb-20 lg:pb-12 px-6 lg:px-16 pt-24 max-w-full">
+      <div className="mt-3 md:mt-6 flex flex-col lg:gap-10 items-center">
+        <h1 className={`${poppins.className} text-4xl lg:text-6xl line-height-lg text-center text-primary font-black  lg:pb-8 uppercase`}>
+          Sobre mí
+        </h1>
+        <div className="flex flex-col gap-8 lg:gap-12 max-w-6xl mb-4 lg:mb-12">
+          <div className="flex lg:gap-8">
+            <Image src={profilePhoto} alt="profile-photo-consultory" className="rounded-2xl hidden lg:block object-cover" width={420} />
+            <div className="flex flex-col gap-4">
+              <h4 className={`${dmSans.className} text-center font-medium text-2xl md:text-4xl lg:text-left leading-10`}>
+                ¡Hola! Soy Daiana Maldonado
+              </h4>
+              <div className="flex flex-col gap-1 md:gap-8 px-10 md:px-0">
+                <h4 className={`${dmSans.className} text-start md:text-center font-medium text-lg md:text-xl lg:text-left leading-10`}>
                   Me egrése de Licenciatura en Nutrición egresada de la Universidad Católica de La Plata en Marzo del 2014. Al finalizar mis estudios,
-                  descubrí que mi pasión es la educación nutricional y nutrición deportiva. Me especialicé en Nutrición Clínica, Nutrición Deportiva
-                  con Francis Holway y Nutrición para vegetarianos/veganos con el Dr. Toto Vivianni Rossi, además de muchas otras capacitaciones y
-                  congresos. Desde que me recibí hasta el día de hoy, atiendo en consultorio particular aplicando empatia, educación alimentaria
-                  nutricional, antropometrías, asesoramiento por planes personales, deportivos adecuándolos a cada persona. Trabajo con asesorías
-                  personalizadas, online para todas las edades para aquellos que no disponen de tiempo para trasladarse o están lejos. También brindo
-                  charlas para empresas, escuelas o otras instituciones que lo requieran. Me apasiona brindar información y transmitir conocimientos
-                  de la nutrición desde una perspectiva mas criolla y simple para comprender. Objetivo Orientado a motivar a que las personas logren
-                  un cambio sostenible en el estilo de vida, mejorar/potenciar el rendimiento deportivo/recreativo.
+                  descubrí que mi pasión es la educación nutricional y nutrición deportiva.
+                </h4>
+                <h4 className={`${dmSans.className} text-start md:text-center font-medium text-lg md:text-xl lg:text-left leading-10`}>
+                  Me especialicé en Nutrición Clínica, Nutrición Deportiva con Francis Holway y Nutrición para vegetarianos/veganos con el Dr. Toto
+                  Vivianni Rossi, además de muchas otras capacitaciones y congresos.
+                </h4>
+                <h4 className={`${dmSans.className} text-start md:text-center font-medium text-lg md:text-xl lg:text-left leading-10`}>
+                  Desde que me recibí hasta el día de hoy, atiendo en consultorio particular aplicando empatia, educación alimentaria nutricional,
+                  antropometrías, asesoramiento por planes personales, deportivos adecuándolos a cada persona. Trabajo con asesorías personalizadas,
+                  online para todas las edades para aquellos que no disponen de tiempo para trasladarse o están lejos. También brindo charlas para
+                  empresas, escuelas o otras instituciones que lo requieran.
+                </h4>
+                <h4 className={`${dmSans.className} text-start md:text-center font-medium text-lg md:text-xl lg:text-left leading-10`}>
+                  Me apasiona brindar información y transmitir conocimientos de la nutrición desde una perspectiva mas criolla y simple para
+                  comprender. Objetivo Orientado a motivar a que las personas logren un cambio sostenible en el estilo de vida, mejorar/potenciar el
+                  rendimiento deportivo/recreativo.
                 </h4>
               </div>
             </div>
-            <div className="my-4 flex flex-col gap-8">
-              <p className={`${poppins.className} text-olive text-4xl`}>Especialidades</p>
-              <div className="columns-1 lg:columns-2 gap-x-0">
-                <ul className="p-8 rounded-xl bg-skyBlue shadow-xl flex flex-col list-disc">
-                  {especialidades.map(({ id, item }) => (
-                    <li key={id} className={`${poppins.className} my-4`}>
-                      <p className="text-lg font-medium">{item}</p>
-                    </li>
-                  ))}
-                </ul>
-                <div className="flex-1 lg:flex justify-end  hidden">
-                  <Image src={avocadoToast} alt="profile-photo-consultory" className="rounded-2xl" width={365} />
-                </div>
-              </div>
+          </div>
+          <div className="my-8 flex flex-col gap-8 ">
+            <p className={`${poppins.className} text-olive md:text-6xl text-4xl text-center`}>Especialidades</p>
+            <div className="flex px-4 md:px-0 lg:gap-8">
+              <ul className="p-8 rounded-xl bg-skyBlue shadow-xl flex flex-col list-disc justify-center">
+                {especialidades.map(({ id, item }) => (
+                  <li key={id} className={`${poppins.className} my-4`}>
+                    <p className="text-xl font-medium">{item}</p>
+                  </li>
+                ))}
+              </ul>
+              <Image
+                src={consul}
+                alt="profile-photo-consultory"
+                className="rounded-2xl hidden lg:block object-cover blur-mds"
+                width={420}
+                placeholder="blur"
+                loading="lazy"
+                onLoad={(e: React.SyntheticEvent<HTMLImageElement, Event>) => e.currentTarget.classList.remove('blur-md')}
+              />
             </div>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
