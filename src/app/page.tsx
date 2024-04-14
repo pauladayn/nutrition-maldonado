0b13e5bfbc7160d0bfa226b1c981c69ee6fcafbe
david <dborgat@gmail.com>
2024-04-14 17:54:08 -0300

    Optimize image loading and fix styling in Home page component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+'use client';
 import Image from 'next/image';
 import heroPhoto from '../../public/assets/profile.jpg';
 import arrowIcon from '../../public/assets/icons/arrow.svg';
@@ -9,6 +10,7 @@ import ServicesSection from '@/components/ServicesSection/ServicesSectionCards';
 import logo from '../../public/assets/logoFlat.png';
 import profilePhoto from '../../public/assets/consul02.jpeg';
 import wave from '../../public/assets/wave.svg';
+//import ImageCustom from '../components/ImageCustom/ImageCustom';
 
 export default function Home() {
   return (
@@ -31,7 +33,15 @@ export default function Home() {
               <Image src={arrowIcon} alt="arrow-icon" width={24} height={24} className="self-center ms-1" />
             </Link>
           </div>
-          <Image src={heroPhoto} alt="hero-profile-photo" width={370} className="rounded-2xl ms-auto me-16 hidden lg:inline-block w-auto h-auto" />
+          <Image
+            src={heroPhoto}
+            loading="lazy"
+            onLoad={(e: React.SyntheticEvent<HTMLImageElement, Event>) => e.currentTarget.classList.remove('blur-md')}
+            placeholder="blur"
+            alt="hero-profile-photo"
+            width={370}
+            className="blur-md rounded-2xl ms-auto me-16 hidden lg:inline-block w-auto h-auto"
+          />
         </div>
       </div>
       {/* planes */}
@@ -88,7 +98,8 @@ export default function Home() {
           <Image src={wave} alt="wave-background-image" />
         </div>
       </div>
-      <div className="flex flex-col max-w-full px-6 lg:px-16 py-6 lg:py-12 gap-8 soft-gradient">
+      {/* sobre mi */}
+      <div className="flex flex-col max-w-full px-6 lg:px-16 py-6 lg:py-12 gap-8 bg-paleGreen">
         <div className="flex align-center flex-col md:flex-row">
           <div className="flex flex-col gap-4 md:gap-8 flex-1 self-center">
             <h1 className={`${poppins.className} text-4xl lg:text-6xl text-olive font-bold`}>¡Hola!</h1>
@@ -97,13 +108,21 @@ export default function Home() {
             </h4>
             <Link
               href="/sobre-mi"
-              className={`${poppins.className} flex items-center justify-center gap-2 btn-padding-lg text-lg rounded-full px-2 py-1 bg-primary text-neutral self-start md:self-end hover:shadow-lg hover-shadow-paleGreen hover:secondary hover:scale-110 md:mx-auto mt-4 md:mt-0`}>
+              className={`${poppins.className} flex items-center justify-center gap-2 btn-padding-lg text-lg rounded-full px-2 py-1 bg-primary text-neutral self-start hover:shadow-lg hover-shadow-paleGreen hover:secondary hover:scale-110 mt-4 md:mt-0`}>
               <span>Más detalles</span>
               <Image src={arrowIcon} alt="arrow-icon" width={24} height={24} className="self-center ms-1" />
             </Link>
           </div>
           <div className="flex-1 lg:flex justify-center hidden">
-            <Image src={profilePhoto} alt="profile-photo-consultory" className="rounded-2xl" width={370} />
+            <Image
+              src={profilePhoto}
+              alt="profile-photo-consultory"
+              className="rounded-2xl blur-md"
+              width={370}
+              loading="lazy"
+              onLoad={(e: React.SyntheticEvent<HTMLImageElement, Event>) => e.currentTarget.classList.remove('blur-md')}
+              placeholder="blur"
+            />
           </div>
         </div>
       </div>
